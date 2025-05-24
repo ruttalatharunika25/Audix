83a2afda035712a14c9606fb1b14b13309638bbb
ruttalatharunika25 <ruttalatharunika25@gmail.com>
2025-05-24 13:21:22 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.route.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import userRouter from "./routes/user.route.js"
+import geminiResponse from "./gemini.js"
+
+
+
+
+
+const app = express()
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
+const port = process.env.PORT || 4000
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
+
+
+  
+
+app.listen(port,()=>{
+  connectDb()
+  console.log("server started")
+})
