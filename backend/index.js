07e5bb287221226162bef7dc843ac630cba0a44c
ruttalatharunika25 <158490271+ruttalatharunika25@users.noreply.github.com>
2025-05-24 15:34:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ import geminiResponse from "./gemini.js"
 
 const app = express()
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://audix-virtual-assistant.onrender.com",
   credentials:true
 }))
 const port = process.env.PORT || 4000
